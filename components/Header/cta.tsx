4ba1f6b87313c4b97abf2e90b6459e2a9e858d04
aryan <agarwalaryan139@gmail.com>
2023-10-11 05:05:06 +0530

    fix: cta link + styling

--- components/Header/cta.tsx
+++ components/Header/cta.tsx
@@ -9,13 +9,13 @@ export const CTALinks = () => {
   return (
     <>
       <Link href={'https://thenetworkstate.com'}>
-        <Text color={pathname === 'https://thenetworkstate.com' ? 'white' : '#C5C5C5'}>TNS Book</Text>
+        <Text color={pathname === 'https://thenetworkstate.com' ? 'white' : '#C5C5C5'} fontSize={"larger"}>TNS Book</Text>
       </Link>
-      <Link href={''}>
-        <Text color={pathname === 'https://twitter.com/startnetworkst' ? 'white' : '#C5C5C5'}>Twitter</Text>
+      <Link href={'https://twitter.com/startnetworkst'}>
+        <Text color={pathname === 'https://twitter.com/startnetworkst' ? 'white' : '#C5C5C5'} fontSize={"larger"}>Twitter</Text>
       </Link>
       <Link href={'/start'}>
-        <Text color={pathname === '/start' ? 'white' : '#FFF'}>
+        <Text color={pathname === '/start' ? 'white' : '#FFF'} fontSize={"2xl"} fontWeight={"extrabold"} border={"3px solid #4931F5"} padding={"0.5rem 1rem"} display="inline-block" >
           Start Building
         </Text>
       </Link>
